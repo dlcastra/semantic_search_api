3874f882633493c4f30e9ae613a74bd061f1de62
dlcastra <1dlcastra@gmail.com>
2025-05-29 15:52:49 +0200

    feature: text tokenization

--- src/embedding/routers.py
+++ src/embedding/routers.py
@@ -1,51 +1,74 @@
 import asyncio
+import uuid
 
-from fastapi import APIRouter, Query
+import tiktoken
+from fastapi import APIRouter, Query, File, UploadFile, Form
 from fastapi.params import Depends
 from pydantic import BaseModel
 
 from src.auth.utils import get_current_user
 from src.clients.azure_openai import embedding_client
 from src.core.settings import settings
+from src.embedding.services import get_text_extractor_service, get_text_tokenization_service
 from src.embedding.vector_db import search_similar, add_embedding, get_all_user_embeddings
 
 router = APIRouter()
 
 
-class TextInput(BaseModel):
+class AddEmbeddingRequest(BaseModel):
+    text: str = Form(None)
+    file: UploadFile = File(None)
+
+
+class SearchEmbeddingRequest(BaseModel):
     text: str
+    limit: int = Form(default=5)
+    score: float = Form(None)
 
 
 @router.post("/add-embedding")
-async def add_embedding_router(text_input: TextInput, auth_payload: dict = Depends(get_current_user)):
+async def add_embedding_router(request_data: AddEmbeddingRequest, auth_payload: dict = Depends(get_current_user)):
     """Add an embedding for the provided text input."""
 
+    tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+    text_extractor = await get_text_extractor_service()
+    text_tokenization = await get_text_tokenization_service(tokenizer, max_tokens=50)
     user_id = auth_payload.get("user").get("sub")
+    text_chunks = []
+
+    if request_data.file:
+        ...
+
+    if request_data.text:
+        text_chunks = await text_tokenization.chunk_text(request_data.text)
 
     response = await asyncio.to_thread(
         embedding_client.embeddings.create,
-        input=[text_input.text],
+        input=text_chunks,
         model=settings.AZURE_OPENAI_DEPLOYMENT_NAME,
     )
 
-    embedding = response.data[0].embedding
-    await add_embedding(vector=embedding, payload={"user_id": user_id, "text": text_input.text})
+    for idx, (chunk, embedding_data) in enumerate(zip(text_chunks, response.data)):
+        embedding = embedding_data.embedding
+        point_id = str(uuid.uuid4())
+        await add_embedding(vector=embedding, payload={"id": point_id, "user_id": user_id, "text": chunk})
 
-    return {"status": "success", "points": embedding}
+    return {"status": "success", "chunks_saved": ""}
 
 
 @router.post("/search-embedding", dependencies=[Depends(get_current_user)])
-async def search_text_embedding_router(text_input: TextInput) -> dict:
+async def search_text_embedding_router(request_data: SearchEmbeddingRequest) -> dict:
     """Search for similar embeddings based on the provided text input."""
 
     response = await asyncio.to_thread(
         embedding_client.embeddings.create,
-        input=[text_input.text],
+        input=[request_data.text],
         model=settings.AZURE_OPENAI_DEPLOYMENT_NAME,
     )
 
     embedding = response.data[0].embedding
-    search_result = await search_similar(vector=embedding, limit=5)
+    search_result = await search_similar(vector=embedding, limit=request_data.limit)
     response = [{"id": r.id, "score": r.score, "text": r.payload.get("text")} for r in search_result]
 
     return {"status": "success", "results": response}
---
+++ src/embedding/services.py
@@ -0,0 +1,150 @@
+from io import BytesIO
+
+import fitz
+from docx import Document
+from nltk import sent_tokenize
+
+from src.core.settings import logger
+
+
+class TextExtractorService:
+    async def extract_text(self, filename: str, file_bytes: BytesIO) -> tuple[str, bool] | tuple[None, bool]:
+        """
+        Extracts text from a file bytes steam based on the file extension.
+
+        :param filename: File name.
+        :param file_bytes: File content as a BytesIO object.
+        :return:
+            - Tuple (`str`, `True`) if the text is extracted successfully.
+            - Tuple (`None`, `False`) if an error occurs or unsupported file type.
+        """
+
+        try:
+            if filename.endswith(".txt"):
+                return await self._extract_text_from_txt(file_bytes)
+            elif filename.endswith(".docx"):
+                return await self._extract_text_from_docx(file_bytes)
+            elif filename.endswith(".pdf"):
+                return await self._extract_text_from_pdf(file_bytes)
+        except Exception as e:
+            logger.error(f"TextExtractorService {str(e)}")
+            return None, False
+
+        return "Unsupported file type", False
+
+    @staticmethod
+    async def _extract_text_from_txt(file_bytes: BytesIO) -> tuple[str, bool] | tuple[None, bool]:
+        """
+        Extracts text from a text file.
+
+        :param file_bytes: File content as a BytesIO object.
+        :return:
+            - Tuple (`str`, `True`) if the text is extracted successfully.
+            - Tuple (`None`, `False`) if an error occurs.
+        """
+
+        try:
+            logger.info(f"Extracting text from TXT file")
+            return file_bytes.getvalue().decode("utf-8"), True
+        except Exception as e:
+            logger.error(f"TextExtractorService {str(e)}")
+            return None, False
+
+    @staticmethod
+    async def _extract_text_from_docx(file_bytes: BytesIO) -> tuple[str, bool] | tuple[None, bool]:
+        """
+        Extracts text from a DOCX file bytes using the `python-docx` library.
+
+        :param file_bytes:  File content as a BytesIO object.
+        :return:
+            - Tuple (`str`, `True`) if the text is extracted successfully.
+            - Tuple (`None`, `False`) if an error occurs.
+        """
+
+        try:
+            logger.info(f"Starting text extraction from DOCX file")
+            doc = Document(file_bytes)
+            result = " ".join([para.text for para in doc.paragraphs if para.text.strip()])
+
+            logger.info(f"Text extracted successfully")
+            return result, True
+        except Exception as e:
+            logger.error(f"TextExtractorService {str(e)}")
+            return None, False
+
+    @staticmethod
+    async def _extract_text_from_pdf(file_bytes: BytesIO) -> tuple[str, bool] | tuple[None, bool]:
+        """
+        Extracts text from a PDF file bytes using the `PyMuPDF` library.
+
+        :param file_bytes: File content as a BytesIO object.
+        :return:
+            - Tuple (`str`, `True`) if the text is extracted successfully.
+            - Tuple (`None`, `False`) if an error occurs
+        """
+
+        try:
+            logger.info(f"Starting text extraction from PDF file")
+            doc = fitz.open("pdf", file_bytes.read())
+            result = " ".join([page.get_text() for page in doc.pages()])
+
+            logger.info(f"Text extracted successfully")
+            return result, True
+        except Exception as e:
+            logger.error(f"TextExtractorService {str(e)}")
+            return None, False
+
+
+class SentenceAwareChunker:
+    def __init__(self, tokenizer, max_tokens: int = 500):
+        self.tokenizer = tokenizer
+        self.max_tokens = max_tokens
+
+    async def count_tokens(self, text: str) -> int:
+        return len(self.tokenizer.encode(text))
+
+    async def chunk_text(self, text: str) -> list[str]:
+        sentences = sent_tokenize(text)
+        chunks = []
+        current_chunk = ""
+        current_tokens = 0
+
+        for sentence in sentences:
+            sentence_tokens = await self.count_tokens(sentence)
+
+            if current_tokens + sentence_tokens <= self.max_tokens:
+                current_chunk += " " + sentence.strip()
+                current_tokens += sentence_tokens
+            else:
+                if current_chunk:
+                    chunks.append(current_chunk.strip())
+
+                if sentence_tokens > self.max_tokens:
+                    token_ids = self.tokenizer.encode(sentence)
+                    for i in range(0, len(token_ids), self.max_tokens):
+                        chunk = self.tokenizer.decode(token_ids[i : i + self.max_tokens])
+                        chunks.append(chunk.strip())
+                    current_chunk = ""
+                    current_tokens = 0
+                else:
+                    current_chunk = sentence.strip()
+                    current_tokens = sentence_tokens
+
+        if current_chunk:
+            chunks.append(current_chunk.strip())
+
+        return chunks
+
+
+async def get_text_extractor_service() -> TextExtractorService:
+    """:return: TextExtractorService instance."""
+    return TextExtractorService()
+
+
+async def get_text_tokenization_service(tokenizer, max_tokens: int = 500) -> SentenceAwareChunker:
+    """
+    :param tokenizer: Tokenizer instance.
+    :param max_tokens: Maximum number of tokens per chunk.
+    :return: SentenceAwareChunker instance.
+    """
+    return SentenceAwareChunker(tokenizer, max_tokens)
